ac55bd0af7b72db15789187409b86c1aaabbba53
1054 <liudz1054@gmail.com>
2017-03-21 14:48:49 +0100

    Updates

--- SETUP.bash
+++ SETUP.bash
@@ -2,7 +2,7 @@
 # 
 
 
-Crab_BIN_SETUP_SCRIPT=$(dirname "$0")/bin/bin_setup.bash
+Crab_BIN_SETUP_SCRIPT=$(dirname "${BASH_SOURCE[0]}")/bin/bin_setup.bash
 
 source "$Crab_BIN_SETUP_SCRIPT" -check casa-ms-split casa-ms-concat pdbi-uvt-go-splitpolar pdbi-uvt-go-import-uvfits pdbi-uvt-go-average pdbi-uvt-go-uvfit
 
