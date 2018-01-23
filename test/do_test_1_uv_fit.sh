23ce2cf4f7e63371d6b2f54e6352729d3b8d810d
1054 <liudz1054@gmail.com>
2018-01-23 16:25:50 +0100

    Updates

---
+++ test/do_test_1_uv_fit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# 
+
+
+echo ""
+echo "This is a test script for running GILDAS uv_fit by calling \"pdbi-uvt-go-uvfit\"."
+echo ""
+
+
+cd test_1_uv_fit
+
+source ../../SETUP.bash
+
+# go uvfit
+pdbi-uvt-go-uvfit -name split_z35_68_spw0_width128.uvt \
+                    -offset 0 0 \
+                    -uvrange 10 2000 \
+                    -subtract -residual output_residual \
+                    -out output_uv_fit
+
+# go uvmap the input image
+pdbi-uvt-go-uvmap -name split_z35_68_spw0_width128.uvt
+
+# go uvmap the residual image
+pdbi-uvt-go-uvmap -name output_residual.uvt
+
+# open the images
+if [[ $(uname) == "Darwin" ]]; then
+open split_z35_68_spw0_width128.eps output_residual.eps
+fi
+
+echo "Done!"
+
+# clean up
+#rm output_* *.lmv* *.eps *.txt *.log *.noi *.beam *.map *.script *~ 2>/dev/null
+
+
