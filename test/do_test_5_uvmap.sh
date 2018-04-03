707a1c282820c9c18c28b0604dd093abd0f35069
1054 <liudz1054@gmail.com>
2018-04-03 19:29:42 +0200

    Updates

---
+++ test/do_test_5_uvmap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# 
+
+
+echo ""
+echo "This is a test script for running GILDAS uv_map by calling \"pdbi-uvt-go-uvmap\"."
+echo ""
+
+
+mkdir test_5_uvmap 2>/dev/null
+
+
+cd test_5_uvmap
+
+
+source ../../SETUP.bash
+
+
+cp ../uv_table_data/split_z35_68_spw0_width128.uvt NAME.uvt
+
+
+pdbi-uvt-go-uvmap -name NAME.uvt \
+                    -size 15 -map_size 512 \
+                    -out OUTPUTNAME
+
+
+echo "Done!"
+
+
