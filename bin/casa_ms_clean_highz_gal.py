424bc3e436521dfa2d6f39f398063f01e0fff2a2
1054 <liudz1054@gmail.com>
2019-06-19 11:48:47 +0200

    Updates

---
+++ bin/casa_ms_clean_highz_gal.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# 
+# This code must be run in CASA
+# 
+
+import os, sys, re, copy, time, datetime
+import numpy as np
+import astropy
+#import python-casacore # brew tap ska-sa/tap; brew search casacore
+
+
+
+
