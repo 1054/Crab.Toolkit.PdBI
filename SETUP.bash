70adef63537651df974ea2bbc0152b98106cc261
1054 <>
2021-01-21 12:10:29 +0100

    Updates

--- SETUP.bash
+++ SETUP.bash
@@ -4,6 +4,10 @@
 
 Crab_BIN_SETUP_SCRIPT=$(dirname "${BASH_SOURCE[0]}")/bin/bin_setup.bash
 
-source "$Crab_BIN_SETUP_SCRIPT" -check casa-ms-split casa-ms-concat pdbi-uvt-go-splitpolar pdbi-uvt-go-import-uvfits pdbi-uvt-go-average pdbi-uvt-go-uvfit
+if [[ $- == *i* ]]; then
+    source "$Crab_BIN_SETUP_SCRIPT" -check casa-ms-split casa-ms-concat pdbi-uvt-go-splitpolar pdbi-uvt-go-import-uvfits pdbi-uvt-go-average pdbi-uvt-go-uvfit
+else
+    source "$Crab_BIN_SETUP_SCRIPT"
+fi
 
 
