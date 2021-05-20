40507a5d108f9c7a04c572c0a8cea624bd907f94
1054 <>
2021-05-20 16:49:03 +0200

    Updates

--- test/do_test_11_print_uvw.sh
+++ test/do_test_11_print_uvw.sh
@@ -20,7 +20,7 @@ source ../../SETUP.bash
 
 #cp ../uv_table_data/split_VUDS0510807732_spw1_width10_SP.uvt NAME.uvt
 
-cp ../uv_table_data/split_z35_68_spw1_width128.uvt NAME_1.uvt # testing single-channel data, for which we also print re im wt
+cp ../uv_table_data/split_z35_68_spw0_width128.uvt NAME_1.uvt # testing single-channel data, for which we also print re im wt
 
 pdbi-uvt-go-splitpolar -name NAME_1.uvt -out NAME.uvt
 
